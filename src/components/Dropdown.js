b08b1883716626b9eb73e28726f800324b3e56ba
Aïssetou SACKO <aissetou.sck@gmail.com>
2022-09-26 14:37:41 +0200

    Dropdown component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import arrowDown from '../assets/arrow-down.png'
+import arrowUp from '../assets/arrow-up.png'
+
+const Dropdown = ({ title, content }) => {
+  const [buttonState, setButtonState] = useState(false)
+
+  return (
+    <div className="dropdown">
+      <button
+        className="dropdown__title"
+        onClick={() => setButtonState(!buttonState)}
+      >
+        {title}
+        <img
+          src={buttonState ? arrowUp : arrowDown}
+          alt="Icone"
+          className="dropdown__icon"
+        />
+      </button>
+      {buttonState && (
+        <div className="dropdown__content">
+          {typeof content === 'object' ? (
+            <ul>
+              {content.map((elt, index) => {
+                return <li key={index}>{elt}</li>
+              })}
+            </ul>
+          ) : (
+            <p>{content}</p>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Dropdown
--- src/pages/Rent.js
+++ src/pages/Rent.js
@@ -1,56 +1,21 @@
 import React /* , { useState, useEffect } */ from 'react'
 import { useParams } from 'react-router-dom'
+import Dropdown from '../components/Dropdown'
 import Slider from '../components/Slider'
 import Stars from '../components/Stars'
 import Tags from '../components/Tags'
 
 const Rent = ({ data }) => {
   const rentId = useParams()
-  /* const [rental, setRental] = useState({})
-
-  useEffect(() => {
-    fetch(`../../data.json`, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data)
-        setRental(
-          data.find((rent) => {
-            return rent.id === rentId.id
-          })
-        )
-      })
-  }, [rentId.id]) */
 
   const accomodation = data.find((item) => item.id === rentId.id)
   console.log(accomodation)
 
-  /* const [firstname, lastname] = rental.host.name.split(' ')
-  console.log('firstname ', firstname) */
   const [firstname, lastname] = accomodation.host.name.split(' ')
-  // console.log('firstname ', firstname)
 
   return (
     <div className="rental">
       <Slider pictures={accomodation.pictures} title={accomodation.title} />
-
-      {/* <div className="rental__information">
-        <div className="rental__title">
-          <p className="rental__title--first">{rental.title}</p>
-          <p className="rental__title--second">{rental.location}</p>
-        </div>
-        <div className="rental__host">
-          <p>
-            {firstname} <br />
-            {lastname}{' '}
-          </p>
-          <img src={rental?.host?.picture} alt="Propriétaire" />
-        </div>
-      </div> */}
 
       <div className="rental__information">
         <div className="rental__title">
@@ -74,8 +39,8 @@ const Rent = ({ data }) => {
       </div>
 
       <div className="rental__description">
-        <h1>DESCRIPTION</h1>
-        <h1>EQUIPEMENTS</h1>
+        <Dropdown title="Description" content={accomodation.description} />
+        <Dropdown title="Équipements" content={accomodation.equipments} />
       </div>
     </div>
   )
